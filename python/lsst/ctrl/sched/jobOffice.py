b3f5bdf2b50d4d3781e7ff2f920a8be4aaee0c9a
rplante <rplante@git.lsstcorp.org>
2010-03-22 14:31:02 +0000

    initial jobOffice

---
+++ python/lsst/ctrl/sched/jobOffice.py
@@ -0,0 +1,15 @@
+"""
+jobOffice implementations
+"""
+from __future__ import with_statement
+
+from blackboard import Blackboard, Props
+
+import os
+
+class JobOffice(object):
+    """
+    an abstract parent class
+    """
+    pass
+
